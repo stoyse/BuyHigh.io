8d0f4d7a6757ab3979b100adee5fb55dcbe90fc9
stoyse <julianfaitdugaming@gmail.com>
2025-05-29 17:32:27 +0200

    made login work

---
+++ BuyHigh/BuyHigh/ViewLogin.swift
@@ -0,0 +1,18 @@
+//
+//  ViewLogin.swift
+//  BuyHigh
+//
+//  Created by Julian Stosse on 29.05.25.
+//
+
+import SwiftUI
+
+struct ViewLogin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewLogin()
+}
