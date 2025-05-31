1a0105e06d000339168393bf1fb5dfa0a83c2636
stoyse <julianfaitdugaming@gmail.com>
2025-05-31 03:08:00 +0200

    added new files for daily quiz feature

---
+++ BuyHigh/BuyHigh/Components/CardDailyQuiz.swift
@@ -0,0 +1,18 @@
+//
+//  CardDailyQuiz.swift
+//  BuyHigh
+//
+//  Created by Julian Stosse on 30.05.25.
+//
+
+import SwiftUI
+
+struct CardDailyQuiz: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardDailyQuiz()
+}
---
+++ BuyHigh/BuyHigh/Load/LoadDailyQuiz.swift
@@ -0,0 +1,8 @@
+//
+//  LoadDailyQuiz.swift
+//  BuyHigh
+//
+//  Created by Julian Stosse on 30.05.25.
+//
+
+import Foundation
