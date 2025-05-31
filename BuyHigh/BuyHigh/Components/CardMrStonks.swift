d4113b589b14fabbb5363e8ac4153f0ea92ab316
stoyse <julianfaitdugaming@gmail.com>
2025-05-31 03:16:52 +0200

    added MrStonks card for the ai then

---
+++ BuyHigh/BuyHigh/Components/CardMrStonks.swift
@@ -0,0 +1,18 @@
+//
+//  CardMrStonks.swift
+//  BuyHigh
+//
+//  Created by Julian Stosse on 31.05.25.
+//
+
+import SwiftUI
+
+struct CardMrStonks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardMrStonks()
+}
