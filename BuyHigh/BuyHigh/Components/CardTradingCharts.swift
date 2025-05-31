ddbcf74ec913c50585309babcff6c6cbe6759790
stoyse <julianfaitdugaming@gmail.com>
2025-05-31 03:30:35 +0200

    create card for trading charts

---
+++ BuyHigh/BuyHigh/Components/CardTradingCharts.swift
@@ -0,0 +1,18 @@
+//
+//  CardTradingCharts.swift
+//  BuyHigh
+//
+//  Created by Julian Stosse on 31.05.25.
+//
+
+import SwiftUI
+
+struct CardTradingCharts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardTradingCharts()
+}
